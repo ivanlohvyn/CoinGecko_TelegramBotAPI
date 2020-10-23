37a6c1b627614357344886fc89da4bf5d9ec7ba8
ivanlohvyn <i.logvyn@gmail.com>
2020-10-23 18:42:43 +0000

    add requests,  write_json

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from flask_sslify import SSLify
+import requests
+import json
 
 
 app = Flask(__name__)
 sslify = SSLify(app)
 
-
-@app.route("/")
-def index():
-    return "<h3>some text</h5>"
+URL = "https://api.telegram.org/bot<YOUR_BOT_TOKEN>/"
 
 
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+def write_json(data, filename="bot_data.json"):
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+
+def main():
+    r = requests.get(URL + "getMe")
+    write_json(r.json())
+
 
 if __name__ == "__main__":
     app.run(debug=True)
